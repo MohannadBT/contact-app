03e588da6fbc3bea03e6f7ce441c69f896324537
MohannadBT <xsmile3x@gmail.com>
2022-11-27 21:08:41 +0300

    end of blades

--- resources/views/contacts/_company-selection.blade.php
+++ resources/views/contacts/_company-selection.blade.php
@@ -1,6 +1,8 @@
 <select class="custom-select">
     <option value="" selected>All Companies</option>
-    <?php foreach($companies as $id => $company): ?>
+
+    @foreach($companies as $id => $company)
     <option value="{{ $id }}">{{ $company['name'] }}</option>
-    <?php endforeach ?>
+    @endforeach
+    
   </select>
--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -32,21 +32,17 @@
                     </thead>
                     <tbody> 
                       @forelse ($contacts as $id => $contact)
-                        <tr>
-                          <th scope="row">1</th>
-                          <td>{{ $contact['name'] }}</td>
-                          <td>{{ $contact['phone'] }}</td>
-                          <td>alfred@test.com</td>
-                          <td>Company one</td>
-                          <td width="150">
-                            <a href="{{ route('contacts.show', $id) }}" class="btn btn-sm btn-circle btn-outline-info" title="Show"><i class="fa fa-eye"></i></a>
-                            <a href="form.html" class="btn btn-sm btn-circle btn-outline-secondary" title="Edit"><i class="fa fa-edit"></i></a>
-                            <a href="#" class="btn btn-sm btn-circle btn-outline-danger" title="Delete" onclick="confirm('Are you sure?')"><i class="fa fa-times"></i></a>
-                          </td>
-                        </tr>
+
+                        {{-- @continue($id == 1)
+                        @break($id == 3) --}}
+
+                        @include('contacts._contact', ['contact' => $contact])
                       @empty
                         <p> No Contacts </p>
                       @endforelse
+
+                      {{-- @each('contacts._contact', $contacts, 'contact', 'contacts._empty')  another approch that doesn't inherite the parent var--}}
+
                     </tbody>
                   </table> 
   
