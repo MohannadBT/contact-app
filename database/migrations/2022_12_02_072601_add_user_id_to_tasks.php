d83c58366d824b4a3a13024644906dfaa5160e24
MohannadBT <xsmile3x@gmail.com>
2022-12-03 10:27:43 +0300

    database 52

--- database/migrations/2022_12_02_072601_add_user_id_to_tasks.php
+++ database/migrations/2022_12_02_072601_add_user_id_to_tasks.php
@@ -14,8 +14,14 @@ return new class extends Migration
     public function up()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->unsignedBigInteger('user_id')->after('id');
-            $table->foreign('user_id')->references('id')->on('users');
+
+            /* Old way */
+            // $table->unsignedBigInteger('user_id')->after('id')->nullable();
+            // $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade'); // u can put ('set null')
+            
+            /* New way */
+            $table->foreignId('user_id')->nullable()->constrained()->cascadeOnUpdate()->cascadeOnUpdate(); 
+            // constrained('taple_name'), u can use nullable or after methods but u need to put them before the constrained 
         });
     }
 
@@ -27,6 +33,8 @@ return new class extends Migration
     public function down()
     {
         Schema::table('tasks', function (Blueprint $table) {
+            // $table->dropForeign('tasks_user_id_foreign');
+            $table->dropForeign(['user_id']); // same as above but this one we specify the column name
             $table->dropColumn('user_id');
         });
     }
