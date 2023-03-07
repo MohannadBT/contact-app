635aecdf38b34d1392bc5f64dc66cfe624b2e015
MohannadBT <xsmile3x@gmail.com>
2023-03-08 02:14:23 +0300

    pagination

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,18 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Repositories\CompanyRepository;
-use App\Models\Contact;
-
 use Illuminate\Http\Request;
+use App\Models\Contact;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class ContactController extends Controller
 {
-    /* 
+    /*
     * another way of getting the repository
 
     protected $company;
 
-    public function __construct(CompanyRepository $company) 
+    public function __construct(CompanyRepository $company)
     {
         $this->company = $company;
     }
@@ -25,23 +25,32 @@ class ContactController extends Controller
 
     public function index()
     {
-        
-        $companies = $this->company->pluck();
-        $contacts = Contact::latest()->get();
 
-        return view('contacts.index', compact('contacts', 'companies')); // compact() is the same as ['contacts' => $contacts] 
+        $companies = $this->company->pluck();
+        // $contacts = Contact::latest()->paginate(10);
+        $contactsCollection = Contact::latest()->get();
+        $perPage = 10;
+        $currentPage = request()->query('page', 1);
+        $items = $contactsCollection->slice(($currentPage * $perPage) - $perPage, $perPage);
+        $total = $contactsCollection->count();
+        $contacts = new LengthAwarePaginator($items, $total, $perPage, $currentPage, [
+            'path' => request()->url(),
+            'query' => request()->query()
+        ]);
+
+        return view('contacts.index', compact('contacts', 'companies')); // compact() is the same as ['contacts' => $contacts]
     }
 
     public function create()
     {
         return view('contacts.create');
     }
-    
+
     public function show($id)
     {
         $contact = Contact::findOrFail($id);
 
-        // abort_unless(!empty($contact), 404); 
+        // abort_unless(!empty($contact), 404);
         // abort_if(!isset($contacts[$id]), 404); //the same as the one above but u change the condition(!issert -> issert)
 
         return view('contacts.show')->with('contact', $contact); // u can send more than 1 value by chaining it like this : ->with()->with()...
--- resources/views/contacts/_contact.blade.php
+++ resources/views/contacts/_contact.blade.php
@@ -1,15 +1,18 @@
 <tr>
     {{-- (if we use @each) --}}
-    <th scope="row">1</th> 
-    
+    <th scope="row">{{ $contacts->firstItem() + $index }}</th>
+
     {{-- <th scope="row">{{ $loop->index }}</th>  --}}
     <td>{{ $contact->first_name }}</td>
     <td>{{ $contact->last_name }}</td>
     <td>{{ $contact->email }}</td>
     <td>{{ $contact->company->name }}</td>
     <td width="150">
-      <a href="{{ route('contacts.show', $contact->id) }}" class="btn btn-sm btn-circle btn-outline-info" title="Show"><i class="fa fa-eye"></i></a>
-      <a href="form.html" class="btn btn-sm btn-circle btn-outline-secondary" title="Edit"><i class="fa fa-edit"></i></a>
-      <a href="#" class="btn btn-sm btn-circle btn-outline-danger" title="Delete" onclick="confirm('Are you sure?')"><i class="fa fa-times"></i></a>
+        <a href="{{ route('contacts.show', $contact->id) }}" class="btn btn-sm btn-circle btn-outline-info"
+            title="Show"><i class="fa fa-eye"></i></a>
+        <a href="form.html" class="btn btn-sm btn-circle btn-outline-secondary" title="Edit"><i
+                class="fa fa-edit"></i></a>
+        <a href="#" class="btn btn-sm btn-circle btn-outline-danger" title="Delete"
+            onclick="confirm('Are you sure?')"><i class="fa fa-times"></i></a>
     </td>
-  </tr>
+</tr>
