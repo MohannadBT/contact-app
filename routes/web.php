7f73f487ebd155a62e997d315373fef1f85aaf63
MohannadBT <xsmile3x@gmail.com>
2023-08-06 20:30:53 +0300

    130 and 131

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\TagController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\Settings\ProfileController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ContactNoteController;
 use App\Http\controllers\WelcomeController;
@@ -28,6 +29,7 @@ Route::fallback(WelcomeController::class);
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/dashboard', DashboardController::class);
+    Route::get('/settings/profile-information', ProfileController::class)->name('user-profile-information.edit');
     Route::resource('/contacts', ContactController::class);
     Route::delete('/contacts/{contact}/restore', [ContactController::class, 'restore'])
         ->name('contacts.restore')
